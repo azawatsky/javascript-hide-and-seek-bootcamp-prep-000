0181de540bedb90aa9a2752c1fa724fd84aa6971
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-06-02 21:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var current = document.querySelectorAll('div#grand-node')
-  console.log(current)
+  console.log(current[0])
   var hold = []
 /*  
   for (let i=0;i<current.length;i++) {
