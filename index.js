2515e891207bc209c0c97df16e201f1b035d8858
Alex Zawatsky <alexzawatsky@yahoo.com>
2019-06-02 22:19:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var rankedChildren = document.querySelectorAll('ul.ranked-list li')
+  for (let i=0;i<rankedChildren.length;i++) {
+    rankedChildren[i].innerHTML = parseInt(rankedChildren[i].innerHTML)+n
+  }
+}
+
+function deepestChild() {
+  var current = document.querySelector('div#grand-node').querySelectorAll('div')
+  return current[current.length-1]
+}
